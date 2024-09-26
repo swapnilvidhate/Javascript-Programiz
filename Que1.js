5d43d357145237bc99c340d7d94e34d0ccba5ce2
swapnilvidhate <103236014+swapnilvidhate@users.noreply.github.com>
2024-09-26 09:11:45 +0530

    delete

--- Que1.js
+++
@@ -1,8 +0,0 @@
-// getting input from user 
-let userInput = prompt("Enter Your Input Value Here")
-
-// converting type of input into number 
-let convertInteger = parseInt(userInput)
-
-// printing the value in console
-console.log("The Integer We Received From User Is", userInput)
